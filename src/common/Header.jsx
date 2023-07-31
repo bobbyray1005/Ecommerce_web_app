bfbe03b34c936fb654c9f1f668e1f3fe4f705d7d
tinker0830 <erin23145@gmail.com>
2023-07-31 20:12:39 +0600

    first update

---
+++ src/common/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component,Fragment } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className='disp'></div>
+            </Fragment>
+        )
+    }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './assets/css/mainfile.css';
 import reportWebVitals from './reportWebVitals';
+import Header from './common/Header';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.headers.common['Authorization'] = 'Bearer '+localStorage.getItem('token');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );
 
